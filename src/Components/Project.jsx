0c4e0fa5a4026bcf14a1fa4dc90b4ad1647301c0
Ismarji Workman <ismarjiw@protonmail.com>
2023-04-17 17:04:11 -0400

    css styling + add project images

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -5,7 +5,8 @@ export default function project(props) {
         <div className="specific-project">
             <h3>{props.name}</h3>
             <h4>{props.description}</h4> 
-            <h4>{props.stack}</h4>
+            <h4 className="project-stack">{props.stack}</h4>
+            <img className="project-image" src={props.image} />
                 <a href={props.demo}>
                     <button>Live Demo <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></button>
                 </a>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Project from "./Project"
+import meme from '../assets/meme.png'
+import mindmeter from '../assets/mindmeter.png'
 
 export default function projects() {
     return (
         <div className="projects-content" id='projects'>
-            <h1>Some Things I've built</h1>
+            <h2>Some Things I've built</h2>
             <Project 
                 name="Mind Meter"
                 description="My first full-stack app. Mind Meter is a web app where users can log meditation sessions, write journal entries, and listen to relaxing music all in one place."
                 stack="React Javascript Python Flask Spotify API YouTube API Google API OAuth2 AWS"
                 demo="http://meetmindmeter.com/"
                 code="https://github.com/ismarjiw/Mind-Meter"
+                image={mindmeter}
             />
             <Project 
                 name="Let me meme it for you"
@@ -18,6 +21,7 @@ export default function projects() {
                 stack="Javascript React HTML CSS Netlify"
                 demo="https://sprightly-blancmange-e1b620.netlify.app/"
                 code="https://github.com/ismarjiw/meme-generator"
+                image={meme}
             />
         </div>
     )
