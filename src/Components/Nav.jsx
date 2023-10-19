3a7948f7ae6bf1649cd218d044f16997859db940
Ismarji Workman <ismarjiw@protonmail.com>
2023-10-19 16:08:56 -0400

    lint test 2

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,56 +1,96 @@
-import { useState, useRef, useEffect }  from "react"
-import { Link } from 'react-scroll'
-import newLogo from '../assets/IW.png'
-import resume from '../documents/Resume.pdf'
+import { useState, useRef, useEffect } from 'react';
+import { Link } from 'react-scroll';
+import newLogo from '../assets/IW.png';
+import resume from '../documents/Resume.pdf';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars as hamburger } from '@fortawesome/free-solid-svg-icons'
+import { faBars as hamburger } from '@fortawesome/free-solid-svg-icons';
 
 export default function Nav() {
-    const [showMenu, setShowMenu] = useState(false);
-    const menuRef = useRef(null);
+	const [showMenu, setShowMenu] = useState(false);
+	const menuRef = useRef(null);
 
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    };
+	const toggleMenu = () => {
+		setShowMenu(!showMenu);
+	};
 
-    const closeMenu = () => {
-        setShowMenu(false);
-    };
+	const closeMenu = () => {
+		setShowMenu(false);
+	};
 
-    useEffect(() => {
-        function handleClickOutside(event) {
-            if (menuRef.current && !menuRef.current.contains(event.target)) {
-                closeMenu();
-            }
-        }
-        if (showMenu) {
-            document.addEventListener("mousedown", handleClickOutside);
-        } else {
-            document.removeEventListener("mousedown", handleClickOutside);
-        }
+	const test = test;
 
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [showMenu]);
+	useEffect(() => {
+		function handleClickOutside(event) {
+			if (menuRef.current && !menuRef.current.contains(event.target)) {
+				closeMenu();
+			}
+		}
+		if (showMenu) {
+			document.addEventListener('mousedown', handleClickOutside);
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside);
+		}
 
-    return (
-        <div className="nav-content" id='hero'>
-            <header>
-                <nav>
-                    <img className="logo" src={newLogo} />
-                    <FontAwesomeIcon icon={hamburger} onClick={toggleMenu}/>
-                    {showMenu && (
-                        <div className="nav-links" ref={menuRef}>
-                            <Link to="hero" spy={true} smooth={true} offset={-100} duration={600} onClick={closeMenu}>Home</Link>
-                            <Link to="about" spy={true} smooth={true} offset={-100} duration={600} onClick={closeMenu}>About</Link>
-                            <Link to="projects" spy={true} smooth={true} offset={-100} duration={600} onClick={closeMenu}>Projects</Link>
-                            <a href={resume} target="_blank" onClick={closeMenu}>Resume</a>
-                            <Link to="contact" spy={true} smooth={true} offset={-100} duration={600} onClick={closeMenu}>Contact</Link>
-                        </div>
-                    )}
-                </nav>
-            </header>
-        </div>
-    )
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [showMenu]);
+
+	return (
+		<div className="nav-content" id="hero">
+			<header>
+				<nav>
+					<img className="logo" src={newLogo} />
+					<FontAwesomeIcon icon={hamburger} onClick={toggleMenu} />
+					{showMenu && (
+						<div className="nav-links" ref={menuRef}>
+							<Link
+								to="hero"
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={600}
+								onClick={closeMenu}
+							>
+								Home
+							</Link>
+							<Link
+								to="about"
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={600}
+								onClick={closeMenu}
+							>
+								About
+							</Link>
+							<Link
+								to="projects"
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={600}
+								onClick={closeMenu}
+							>
+								Projects
+							</Link>
+							<a href={resume} target="_blank" onClick={closeMenu} rel="noreferrer">
+								Resume
+							</a>
+							<Link
+								to="contact"
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={600}
+								onClick={closeMenu}
+							>
+								Contact
+							</Link>
+						</div>
+					)}
+				</nav>
+			</header>
+		</div>
+	);
 }
