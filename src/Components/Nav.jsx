b6d9b1bbed9cc8edd5fc359de625ae020a125e47
Ismarji Workman <ismarjiw@protonmail.com>
2023-10-19 15:37:40 -0400

    add hamburger menu for nav link

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,26 +1,56 @@
-import React from "react"
-import {Link} from 'react-scroll'
-import portLogo from '../assets/logo.png'
+import { useState, useRef, useEffect }  from "react"
+import { Link } from 'react-scroll'
 import newLogo from '../assets/IW.png'
 import resume from '../documents/Resume.pdf'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars as hamburger } from '@fortawesome/free-solid-svg-icons'
 
 export default function Nav() {
+    const [showMenu, setShowMenu] = useState(false);
+    const menuRef = useRef(null);
 
-    return ( 
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    const closeMenu = () => {
+        setShowMenu(false);
+    };
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                closeMenu();
+            }
+        }
+        if (showMenu) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [showMenu]);
+
+    return (
         <div className="nav-content" id='hero'>
-        <header>
-        <nav> 
-            <img className="logo" src={newLogo} />
-            <div className="nav-links">
-                <Link to="hero" spy={true} smooth={true} offset={-100} duration={600}>Home</Link>
-                <Link to="about" spy={true} smooth={true} offset={-100} duration={600}>About</Link>
-                <Link to="projects" spy={true} smooth={true} offset={-100} duration={600}>Projects</Link>
-                <a href={resume} target="_blank">Resume</a>
-                <Link to="contact" spy={true} smooth={true} offset={-100} duration={600}>Contact</Link>
-            </div>
-            
-        </nav>
-        </header>
+            <header>
+                <nav>
+                    <img className="logo" src={newLogo} />
+                    <FontAwesomeIcon icon={hamburger} onClick={toggleMenu}/>
+                    {showMenu && (
+                        <div className="nav-links" ref={menuRef}>
+                            <Link to="hero" spy={true} smooth={true} offset={-100} duration={600} onClick={closeMenu}>Home</Link>
+                            <Link to="about" spy={true} smooth={true} offset={-100} duration={600} onClick={closeMenu}>About</Link>
+                            <Link to="projects" spy={true} smooth={true} offset={-100} duration={600} onClick={closeMenu}>Projects</Link>
+                            <a href={resume} target="_blank" onClick={closeMenu}>Resume</a>
+                            <Link to="contact" spy={true} smooth={true} offset={-100} duration={600} onClick={closeMenu}>Contact</Link>
+                        </div>
+                    )}
+                </nav>
+            </header>
         </div>
     )
 }
