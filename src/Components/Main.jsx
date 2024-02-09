34da57ed9ce3a54cabf67b30aef9f7ca35d8d9d8
Ismarji Workman <ismarjiw@protonmail.com>
2024-02-09 16:21:54 -0500

    remove mindmeter link, add projects, update about me section

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -19,17 +19,21 @@ export default function Main() {
             <div className="about-avatar">
                 <section className="about-resume">
                     <h2 id='about'> About Me </h2>
-                    <h3>Hello, I'm Ismarji <i>(is-mar-e)</i>, a web developer with a passion for both code and delicious KBBQ! <span className="food">🍖</span></h3>
-                    <h3>My journey into the world of web development began in 2011 when I took my first steps in learning JavaScript through Codecademy.</h3>
-                    <h3>Fast forward to today, I've had the incredible opportunity to be sponsored by Walmart to join <a href="https://hackbrightacademy.com/">Hackbright Academy</a> where I completed their full-stack software engineering program and proudly graduated in January 2023. Now, my main focus is creating accessible and inclusive digital products that combine my expertise in front-end and back-end design.</h3>
+                    <h3>Hello! I'm Ismarji <i>(is-mar-e)</i>, a former ophthalmic technician and surgery coordinator with six years of healthcare experience, now immersed in the world of tech <span className="glasses">🤓</span></h3>
+                    <h3>Known for my meticulous approach and precision, I bring a unique blend of healthcare expertise and a relentless commitment to excellence to every task I undertake. With an innate ability to master diverse responsibilities, I'm poised to make substantial contributions to cutting-edge projects within the tech industry.</h3>
+                    <h3>Armed with a Bachelor of Science in Biology from the University of Georgia and a graduate of <a href="https://hackbrightacademy.com/">Hackbright Academy's</a> rigorous, 12-week full-stack software engineering bootcamp, I'm fully equipped to make a positive impact in the tech industry with the skills I've acquired along my tech journey.</h3>
+                    <h3>If you're curious to learn more about me or explore my work, let's connect! Let's collaborate and create something extraordinary together <span className="rocket">🚀</span></h3>
+                    <h3>In my free time, when I'm not coding, you can find me swimming <span className="swimming">🏊🏽‍♀️</span>, playing chess, or diving into a new fiction novel.</h3>
                     <h3>Here are a few technologies I've been working with recently:</h3>
                     <ul>
                         <li>JavaScript</li>
                         <li>React</li>
+                        <li>Angular</li>
                         <li>Python</li>
                         <li>Flask</li>
+                        <li>FastAPI</li>
                         <li>Java</li>
-                        <li>Springboot</li>
+                        <li>Spring Boot</li>
                     </ul>
                     <button onClick={openResume}>Download Resume <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 20" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg></button>
                 </section>
@@ -53,6 +57,5 @@ export default function Main() {
             </div>
         </div>
         </div>
-        
     )
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,37 +3,46 @@ import Project from "./Project"
 import meme from '../assets/meme.png'
 import mindmeter from '../assets/mindmeter.png'
 import listify from '../assets/listify.png'
+import summarizer from '../assets/summarizer.png'
 
 export default function projects() {
     return (
         <div className="projects-content" id='projects'>
             <h2>Some Things I've Built</h2>
             <Project 
+                name="Text Summarizer"
+                description="A web app that allows users to generate summaries from large text using OpenAI's GPT-3.5 Turbo model."
+                stack="JavaScript React CSS Python FastAPI"
+                code="https://github.com/ismarjiw/text-summarizer"
+                image={summarizer}
+                altText="A screenshot of the frontpage of Text Summarizer"
+            />
+            <Project 
                 name="Listify"
                 description="A web app that allows users to have a smart shopping list that learns their buying habits and helps them remember what they are likely to need to buy on their next trip to the store."
                 stack="JavaScript React TailwindCSS Firebase"
-                demo="https://tcl-64-smart-shopping-list.web.app/"
+                // demo="https://tcl-64-smart-shopping-list.web.app/"
                 code="https://github.com/the-collab-lab/tcl-64-smart-shopping-list"
                 image={listify}
                 altText="A screenshot of the frontpage of Listify"
             />
             <Project 
-                name="Mind Meter"
-                description="A web app where users can log meditation sessions, write journal entries, and listen to relaxing music all in one place."
-                stack="JavaScript React TailwindCSS Python Flask Spotify API Google API AWS"
-                demo="http://meetmindmeter.com/"
-                code="https://github.com/ismarjiw/Mind-Meter"
-                image={mindmeter}
-                altText="A screenshot of the frontpage of Mind Meter"
-            />
-            <Project 
                 name="Let me meme it for you"
                 description="A web app that allows users to generate random meme images to add text to and create their own custom memes."
                 stack="JavaScript React HTML CSS"
-                demo="https://letmememeitforyou.netlify.app/"
+                // demo="https://letmememeitforyou.netlify.app/"
                 code="https://github.com/ismarjiw/meme-generator"
                 image={meme}
                 altText="A screenshot of the frontpage of Let me meme it for you"
+            />
+            <Project 
+                name="Mind Meter"
+                description="A web app where users can log meditation sessions, write journal entries, and listen to relaxing music all in one place."
+                stack="JavaScript React TailwindCSS Python Flask Spotify API Google API AWS"
+                // demo="http://meetmindmeter.com/" // formerly hosted here 
+                code="https://github.com/ismarjiw/Mind-Meter"
+                image={mindmeter}
+                altText="A screenshot of the frontpage of Mind Meter"
             />
         </div>
     )
